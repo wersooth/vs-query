0984bbc7e7ea0308eb1324d3b7829123a5e86223
Serynooth Wersuken <angel.lacy@gmail.com>
2025-09-02 21:43:17 +0200

    Fix time display under 10m

--- src/vs_query/routes.py
+++ src/vs_query/routes.py
@@ -3,6 +3,7 @@ from flask import render_template
 from vs_query import VERSION, VS_URL
 import  requests
 import logging
+from dateutil.parser import parse
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -25,13 +26,16 @@ MONTH = ["Jan",
 @app.route('/')
 def index():
     rsp = query_vs()
-    return render_template('index.html',ver=VERSION, url=VS_URL, RESP=rsp, MONTH=MONTH)
+    vs_time = parse("{}:{}".format(rsp['date']['hour'],rsp['date']['minute'])).strftime("%H:%M")
+    return render_template('index.html',ver=VERSION, url=VS_URL, RESP=rsp, MONTH=MONTH,TIME=vs_time)
 
 
 def query_vs() -> list:
+    import time
     _url = VS_URL
     try:
         response = requests.get(VS_URL)
+        resp = response.json()
     except Exception as ex:
         logger.error(ex)
     logging.info(f"Query VS instance: "+ VS_URL)
